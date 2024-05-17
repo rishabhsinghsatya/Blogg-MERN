3407717b99e26ddc137a8190745b6d50c73c1904
Rishabh Singh <vineetsatya36@gmail.com>
2024-05-17 23:15:39 +0530

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import CreateBlogPage from "./pages/CreateBlogPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/create-blog" element={<CreateBlogPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
